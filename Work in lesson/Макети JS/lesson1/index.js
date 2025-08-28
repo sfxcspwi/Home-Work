58ed0e092d9324f10cc0c95f0e93c692d49d9f9e
sfxcspwi <staschuguivec@gmail.com>
2025-08-28 20:49:37 +0300

    all

---
+++ Work in lesson/Макети JS/lesson1/index.js
@@ -0,0 +1,25 @@
+console.log("hello world");
+let currentH=("Poltava");
+let a;
+let b;
+let c=12;
+let d=23;
+let e=34;
+
+
+const ageM=16;
+const ageF=16;
+const currentT=19;
+const currentD=("Tuesday");
+const currentM=("August");
+
+
+a=4+5;
+b=2*a;
+
+console.log(a);
+console.log(b);
+console.log(a, b, c, d, e);
+console.log(ageM+ageF);
+console.log(ageM*ageF);
+console.log(currentH, currentM, currentD, currentT);
---
+++ Work in lesson/Макети JS/lesson2/index.js
@@ -0,0 +1,27 @@
+// let a=3;
+// console.log(typeof a);
+// alert("Hello");
+// confirm("Hello");
+// let b=prompt("Hello");
+// console.log(b)
+
+// let a="Привіт";
+// let b=123;
+// let c=true;
+// let d="true";
+// console.log(typeof a, typeof b, typeof c, typeof d,);
+
+// let userN=prompt("Вкажіть ваше ім'я:");
+// console.log(userN);
+
+// let group="FE_28_24_08_24";
+// console.log( `Я навчаюся в GoITeens в групі:${group}`);
+
+// let name="MacBook Pro 13";
+// let price=10000;
+// console.log( `Обрано «${name}», Ціна за штуку ${price} кредитів `);
+// price=20000; 
+// console.log( `Обрано «${name}», Ціна за штуку ${price} кредитів `);
+
+//let userName=prompt("Привіт, як вас звати?");
+//alert (`${userName}, дякуємо що приєднався`);
