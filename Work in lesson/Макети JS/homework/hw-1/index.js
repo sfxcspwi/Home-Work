e98e8cf1d2a5420cd86cb62601c58d821294dcae
sfxcspwi <staschuguivec@gmail.com>
2025-08-28 20:49:04 +0300

    hw

---
+++ Work in lesson/Макети JS/homework/hw-1/index.js
@@ -0,0 +1,24 @@
+let age=16;
+console.log(age)
+
+let name="Станіслав";
+console.log(name);
+
+let isStudent=true;
+console.log(isStudent);
+
+let myString="Чужі помилки легше побачити, чим свої";
+console.log(myString);
+
+let myNull=null;
+console.log(myNull);
+
+let userName=prompt("Введіть своє ім'я");
+alert(userName);
+console.log(typeof userName);
+
+let userAnswer=confirm("Ви дійсно хочете покинути сторінку?");
+console.log(userAnswer);
+
+alert("Увага! Дія може бути небезпечною!");
+confirm("Ви впевнені, що хочете продовжити?");
