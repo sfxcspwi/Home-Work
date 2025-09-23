f27f159fc9f3eb928567f8c134b83b01e507d6c8
sfxcspwi <staschuguivec@gmail.com>
2025-09-23 15:03:27 +0300

    hw

---
+++ Work in lesson/Макети JS/homework/hw-3/index.js
@@ -0,0 +1,15 @@
+let result=5+5+'5';
+console.log(result);
+
+let email="staschuguivec@icloud.com";
+console.log(email.length, email.includes("@"));
+
+let a = "My";
+let b = "name";
+let c = "is";
+let fullName = a + ' ' +  b + ' ' +  c + ' ' + "Stanislav";
+console.log(fullName);
+
+let userName = "Stanislav";
+let payment = 300;
+alert(` Дякуємо, ${userName}! До сплати ${payment} гривень`);
