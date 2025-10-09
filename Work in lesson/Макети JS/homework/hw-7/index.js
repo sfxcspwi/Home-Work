9eeb93391f18d9fa5d5378dc8de882db74fc3a00
sfxcspwi <staschuguivec@gmail.com>
2025-10-09 21:05:59 +0300

    hw

---
+++ Work in lesson/Макети JS/homework/hw-7/index.js
@@ -0,0 +1,55 @@
+// let Arr = [ 1, 2 , 3]
+// Arr[1]=100;
+// console.log(Arr);
+
+// let Arr = [ "Stas", "Chuguivets", "Oleksandrovich"]
+// Arr.push="Dykanka";
+// console.log(Arr);
+
+// const numbers = [1, 5, 8, 9, 12, 4, 15, 27, 30, 18, 14];
+// let total = 0;
+// for (n=0; n<numbers.length; n+=1)
+// {
+//     if (numbers[n] % 2 === 0 )
+//     {
+//         total += numbers[n]; 
+//     }
+// }
+
+// console.log(total);
+
+// const Arr = [1, 2, 3, 4, 5];
+// for(const element of Arr)
+// {
+//     console.log(element)
+// }
+
+// const string = ["Stas", "Stanislav", "Chu", "Chuguivets"];
+// for (const element of string)
+// {
+//     if (element.length>5)
+//     {
+//         console.log(element)
+//     }
+// }
+
+// const numbers = [51, 18, 13, 24, 7, 85, 19];
+// let biggestNumber = numbers[0];
+// for (n=0; n<numbers.length; n+=1)
+// {
+//     if (numbers[n] > biggestNumber)
+//     {
+//         biggestNumber = numbers[n];
+//     }
+// }
+// console.log(biggestNumber)
+
+const Arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for( const element of Arr)
+{
+    if (element % 2 === 0)
+    {
+        console.log(element)
+    }
+}
